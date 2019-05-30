9aab907192a57c14fcfce460597857534d26d7f3
shisongyan <songyan_shi@163.com>
2019-05-30 23:34:38 +0800

    添加一点点样式

--- src/pages/ImageToText/index.tsx
+++ src/pages/ImageToText/index.tsx
@@ -4,7 +4,11 @@ import ImageToText from '@/Components/image-to-text'
 
 const style ={
   marginTop: 32,
+  padding: 16,
+  background: 'rgba(0, 0, 0, 0.03)',
 }
+
+const lang = ['chi_sim', 'eng']
 export default class index extends React.PureComponent {
 
   state = {
@@ -25,13 +29,13 @@ export default class index extends React.PureComponent {
     const { list } = this.state
     return (
       <div>
-        <ImageToText onChange={this.onChange} lang={['chi_sim', 'eng']} />
+        <ImageToText onChange={this.onChange} lang={lang} />
         {list.map((item: any, index: number) => {
           const { status, progress, text } = item
           if (!status) {
             return <div style={style} key={index}>{text}</div>
           }
-          return <div style={style} key={index}>{status}: {progress*100}%</div>
+          return <div style={style} key={index}>{status}: {progress.toFixed(4)*100}%</div>
         })}
       </div>
     )
