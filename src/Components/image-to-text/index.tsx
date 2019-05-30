e6ad5c865cf124443a46afef9932d7add83c59d8
shisongyan <songyan_shi@163.com>
2019-05-30 21:32:52 +0800

    图片识别基本功能,还有bug

--- src/Components/image-to-text/index.tsx
+++ src/Components/image-to-text/index.tsx
@@ -6,28 +6,55 @@ const worker = new TesseractWorker()
 
 interface Props {
   lang?: Array<string> | string;
+  onChange: Function;
 }
 
 export default class ImageToText extends React.PureComponent<Props> {
 
-  state = {
-
+  static defaultProps = {
+    lang: 'chi_sim',
+    onChange: () => {},
   }
 
   handlePreview = () => {
 
   }
 
-  handleChange = () => {
+  imgData: any = {
+
+  }
+
+  handleChange = ({file, fileList}) => {
+    const { onChange, lang } = this.props
+    const { uid, status, originFileObj } = file
+    const language = Array.isArray(lang) ? lang.join("+"): lang
+    if (status === 'done') {
+      worker.recognize(originFileObj, language)
+        .progress((message: any) => {
+          this.imgData[uid] = message
+          onChange(this.handleImageData())
+        })
+        .then((result:any) => {
+          this.imgData[uid] = result
+          onChange(this.handleImageData())
+        })
+    }
+    if (status === 'removed') {
+      delete this.imgData[uid]
+      onChange(this.handleImageData())
+    }
     
+  }
+
+  handleImageData() {
+    return Object.keys(this.imgData).map(uid => this.imgData[uid])
   }
 
   render() {
     return (
       <Upload
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        multiple
         listType="picture-card"
-        // fileList={fileList}
         onPreview={this.handlePreview}
         onChange={this.handleChange}
       >
--- src/pages/ImageToText/index.tsx
+++ src/pages/ImageToText/index.tsx
@@ -1,21 +1,38 @@
 import React from 'react'
 import ImageToText from '@/Components/image-to-text'
-import Image from './WechatIMG38.png'
 
+
+const style ={
+  marginTop: 32,
+}
 export default class index extends React.PureComponent {
 
   state = {
-
+    list: [],
   }
 
   componentDidMount() {
     
   }
 
+  onChange = (list: any) => {
+    this.setState({
+      list,
+    })
+  }
+
   render() {
+    const { list } = this.state
     return (
       <div>
-        <ImageToText lang={['chi_sim', 'eng']} />
+        <ImageToText onChange={this.onChange} lang={['chi_sim', 'eng']} />
+        {list.map((item: any, index: number) => {
+          const { status, progress, text } = item
+          if (!status) {
+            return <div style={style} key={index}>{text}</div>
+          }
+          return <div style={style} key={index}>{status}: {progress}</div>
+        })}
       </div>
     )
   }
