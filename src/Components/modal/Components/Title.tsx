58125b83c8fa0792f7f6d4a38c6e1e1d863ae6eb
shisongyan <songyan_shi@163.com>
2019-06-04 10:34:03 +0800

    拖动功能完善  增加拖拽边界  状态重置

--- src/Components/modal/Components/Title.tsx
+++ src/Components/modal/Components/Title.tsx
@@ -1,63 +1,106 @@
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import cls from 'classnames'
 import { Icon } from 'antd'
 
 
 interface Props {
+  getPos: Function;
   isFull: boolean;
   setFull: Function;
   color?: string;
   fullable: boolean;
   dragable: boolean;
   children: any;
-  modalInstance: any;
 }
 
 export default class Title extends React.PureComponent<Props> {
 
   state = {
-
+    moving: false,
   }
+
+  lastDisX: number = 0
+
+  lastDisY: number = 0
 
   mouseDownPosX: number = 0
 
   mouseDownPosY: number = 0
 
   componentWillUnmount() {
-    document.removeEventListener('mouseup', this.mouseUp)
+    document.removeEventListener('mouseup', this.onMouseUp)
+    document.removeEventListener('mousemove', this.onMouseMove)
+  }
+
+  componentDidMount() {
+    document.addEventListener('mouseup', this.onMouseUp)
+    document.addEventListener('mousemove', this.onMouseMove)
   }
 
   onMouseDown = (e: any) => {
-    const { dragable, modalInstance, isFull } = this.props
-    if (!dragable || !modalInstance || isFull) return 
-    const modalElement = ReactDOM.findDOMNode(modalInstance)
-    if (!modalElement) return 
-    const target = modalElement.querySelector('.ant-modal')
+    const { dragable, isFull } = this.props
+    if (!dragable || isFull) return 
     const { clientX, clientY } = e
-    this.mouseDownPosX = clientX
-    this.mouseDownPosY = clientY
-    document.addEventListener('mousemove', this.mouseMove)
+    this.mouseDownPosX = clientX - this.lastDisX
+    this.mouseDownPosY = clientY - this.lastDisY
+    this.setState({
+      moving: true,
+    })
   }
 
   mouseMove = (e: any) => {
     const { clientX, clientY } = e
     const { getPos } = this.props
-    const disX = clientX - this.mouseDownPosX
-    const disY = clientY - this.mouseDownPosX
-    getPos(disX, disY)
-    document.addEventListener('mouseup', this.mouseUp)
+    const isValid = this.calcBoundary(clientX, clientY)
+    if (!isValid) return 
+    const disX = clientX - this.mouseDownPosX 
+    const disY = clientY - this.mouseDownPosY 
+    this.lastDisX = disX
+    this.lastDisY = disY
+    getPos(
+      {translateX: disX, translateY: disY}, 
+    )
   }
 
-  mouseUp = () => {
-    document.removeEventListener('mousemove', this.mouseMove)
+  onMouseUp = () => {
+    this.setState({
+      moving: false,
+    })
+  }
+
+  onMouseMove = (e: any) => {
+    const { moving } = this.state
+    if (!moving) return
+    this.mouseMove(e)
+  }
+
+  calcBoundary(x: number, y: number): boolean {
+    const boundaryTop: number = 0
+    const boundaryLeft: number = 0
+    const boundaryRight: number = innerWidth
+    const boundaryBottom: number = innerHeight
+    if (
+      x > boundaryLeft &&
+      x < boundaryRight &&
+      y > boundaryTop &&
+      y < boundaryBottom
+    ) {
+      return true
+    }
+    return false
   }
 
   render() {
+    const { moving } = this.state
     const { isFull, fullable, setFull, children, color } = this.props
     if (!children) return null
     return (
-      <div className="title-main" style={{background:color}} onMouseDown={this.onMouseDown}>
+      <div 
+        style={{background:color}} 
+        className={cls("title-main",{moving})}
+        onMouseDown={this.onMouseDown}
+      >
         <div>{children}</div>
         {fullable&&(
           <div className="full-screen-icon-wrapper" onClick={setFull}>
--- src/Components/modal/index.tsx
+++ src/Components/modal/index.tsx
@@ -4,7 +4,6 @@ import { Modal as AntdModal } from 'antd'
 import cls from 'classnames'
 import ModalBody from './Components/ModalBody'
 import Title from './Components/Title'
-import { fail } from 'assert';
 
 
 
@@ -41,11 +40,11 @@ export default class Modal extends React.PureComponent<Props> {
     this.setState({
       isFull: !isFull,
     }, () => {
-      this.setClass(!isFull)
+      this.setFullScreenClass(!isFull)
     })
   }
 
-  setClass = (isFull: boolean) => {
+  setFullScreenClass = (isFull: boolean) => {
     if (!this.modalInstance) return 
     const modalElement = ReactDOM.findDOMNode(this.modalInstance)
     if (!modalElement) return 
@@ -67,36 +66,43 @@ export default class Modal extends React.PureComponent<Props> {
     this.setState({
       transform: null,
     })
+    this.titleInstance.lastDisX = 0
+    this.titleInstance.lastDisY = 0
     onCancel()
   }
 
-  getPos = (disX, disY) => {
+  getPos = (
+    translate: {translateX: number, translateY: number}
+    ) => {
+    const { translateX, translateY } = translate
     this.setState({
-      transform: `translate(${disX}px, ${disY}px)`
+      transform: `translate(${translateX}px, ${translateY}px)`,
     })
   }
+
+  titleInstance: any = null
 
   modalInstance: any = null
 
   render() {
     const { isFull, transform } = this.state
-    console.log(transform)
+    const trans = isFull? null: transform
     const { className, dragable, fullable, title, headerColor, children, ...otherProps } = this.props
     return (
       <AntdModal 
         // destroyOnClose
-        style={{transform}}
+        style={{transform:trans}}
         centered
         ref={instance => {this.modalInstance=instance}}
         title={(
           <Title 
+            ref={instance => {this.titleInstance=instance}}
             color={headerColor}
             getPos={this.getPos}
             isFull={isFull}
             setFull={this.setFull}
             fullable={fullable} 
             dragable={dragable}
-            modalInstance={this.modalInstance}
           >
             {title}
           </Title>
